db9f623ae925a3f7262c4652f51a211db760d67c
DavidJGChen <dchen1@andrew.cmu.edu>
2020-03-31 21:46:30 -0400

    Added jaw movement

---
+++ Assets/Scripts/JawController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class JawController : MonoBehaviour
+{
+    public Transform Jaw;
+    public Transform JawStartPoint;
+    public Transform JawEndPoint;
+    [SerializeField]
+    private float jawSpeed;
+    [SerializeField]
+    private float jawAccelerationTime;
+    private float mouseVelocityY;
+    private float currentJawVelocityY;
+    private float refJawVelocityY;
+    private float jawStartY;
+    private float jawEndY;
+    private float jawCurrentY;
+    private bool jawOpen;
+
+    private void Start() {
+        jawStartY = JawStartPoint.localPosition.y;
+        jawEndY = JawEndPoint.localPosition.y;
+    }
+
+    private void Update() {
+        mouseVelocityY = Input.GetAxis("Mouse Y");
+    }
+
+    private void FixedUpdate() {
+        Vector2 newPos = Jaw.localPosition;
+
+        // currentJawVelocityY = 
+        //     Mathf.SmoothDamp(currentJawVelocityY,
+        //         mouseVelocityY * jawSpeed, 
+        //         ref refJawVelocityY, 
+        //         jawAccelerationTime);
+
+        // newPos.y += currentJawVelocityY * Time.deltaTime;
+        newPos.y += mouseVelocityY * jawSpeed * Time.deltaTime;
+        newPos.y = Mathf.Clamp(newPos.y, jawEndY, jawStartY);
+
+        if (jawStartY - newPos.y < 0.05f) {
+            if (jawOpen) {
+                OnJawClose();
+                jawOpen = false;
+            }
+        }
+        else {
+            jawOpen = true;
+        }
+
+        Jaw.localPosition = newPos;
+    }
+
+    private void OnJawClose() {
+        print("nom");
+    }
+}
